6df80689d566f2c9c81e1219fb50fb7dae25401f
UnrequitedEcho <139258164+UnrequitedEcho@users.noreply.github.com>
2024-12-08 08:45:50 +0100

    Small day7 refinements

--- Day7/day7.py
+++ Day7/day7.py
@@ -39,40 +39,11 @@ def solve(equations, part_nb):
                 
     print(solution)
 
-def part2(equations):
-    solution = 0
-    for expected, numbers in tqdm(equations.items()):
-        nodes = [Node(numbers[0])]
-        numbers = numbers[1:]
-        found = False
-        while not found and numbers :
-            for node in list(nodes):
-                node.add = Node(node.res + numbers[0])
-                if node.add.res == expected:
-                    found = True
-                node.mul = Node(node.res * numbers[0])
-                if node.mul.res == expected:
-                    found = True
-                node.conc = Node()
-                if node.conc.res == expected:
-                    found = True
-                nodes.remove(node)
-                nodes.append(node.add)
-                nodes.append(node.mul)
-                nodes.append(node.conc)
-            numbers = numbers[1:]
-
-        if found:
-            solution += expected
-    print(solution)
-
 equations = []
 with open(Path(__file__).parent / "input.txt", "r") as input:
     for line in input:
         equations.append([int(line.split(":")[0])] + [int(x) for x in line.split(":")[1].strip().split(" ")])
 
-#equations = [[292, 11, 6, 16, 20]]
 solve(equations, part_nb=1)
 solve(equations, part_nb=2)
-#part2(equations) 
 
