f763f4e41b9b9150f4255b48bb8b859b7a6482bd
UnrequitedEcho <139258164+UnrequitedEcho@users.noreply.github.com>
2024-12-09 18:58:39 +0100

    Repository Reorganisation

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Advent of Code 2024
 UnrequitedEcho's solutions to the advent of code 2024 competition : https://adventofcode.com/2024/
-  
-Each "day" directory contains :
-* input.txt : The input for the problem of the day
-* day*.py : My first solution, with very little optimisation and cleanup
-* Optionally, a day*.xxx : A "better" or more challenging solution in an other language
--- build.py
+++ build.py
@@ -4,42 +4,32 @@ import subprocess
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("first", help="first day to build, only day if last is not specified", type=int)
-parser.add_argument("-l", "--last", help="will build every day from first to last included", type=int)
+parser.add_argument("day_nb", help="number of the day to build", type=int)
 parser.add_argument("-pr", "--profileRelease", help="build without debug info and with -O2", action="store_true")
 parser.add_argument("-r", "--run", help="run after building", action="store_true")
 args = parser.parse_args()
 
-if args.last and args.last > args.first:
-    work_on_day = [i for i in range(args.first, args.last + 1)]
+day = str(args.day_nb)
+path = Path(__file__).parent
+source = path / ("day" + day + ".cpp")
+object_file = path / ("day" + day + ".o")
+output = path / ("day" + day)
+
+print("Building day" + day + ".cpp ...")
+if args.profileRelease:
+    compil_args = ["g++", "-c", str(source), "-o", str(object_file), "-O2"]
 else:
-    work_on_day = [args.first]
+    compil_args = ["g++", "-c", str(source), "-o", str(object_file), "-g", "-Og", "-Wall", "-Wextra"]
+result = subprocess.run(compil_args)
 
-for day in work_on_day:
-    source = Path(Path(__file__).parent.as_posix() + "/Day" + str(day) + "/day" + str(day) + ".cpp")
-    object_file = Path(source).with_suffix(".o")
-    output = Path(source).with_suffix("")
+if result.returncode != 0:
+    exit()
 
-    build_error = True
-    link_error = True
-    
-    print("Building day" + str(day) + ".cpp ...")
-    if args.profileRelease:
-        build_error = subprocess.call(["g++", "-c", source.as_posix(), "-o", object_file.as_posix(), "-O2"])
-    else:
-        build_error = subprocess.call(["g++", "-c", source.as_posix(), "-o", object_file.as_posix(), "-g", "-Og", "-Wall", "-Wextra"])
-    if build_error:
-        print("Build Error : ", build_error)
-        exit()
-    
-    print("Linking day" + str(day) + ".o ...")
-    link_error = subprocess.call(["g++", object_file.as_posix(), "-o", output])
-    if link_error:
-        print("Link Error : ", link_error)
-        object_file.unlink(missing_ok = True)
-        exit()
-    object_file.unlink()
-    
-    if args.run:
-        result = subprocess.call([output.as_posix()])
+print("Linking day" + day + ".o ...")
+result = subprocess.run(["g++", str(object_file), "-o", str(output)])
+
+Path(str(object_file)).unlink(missing_ok=True)
+
+if result.returncode == 0 and args.run:
+    result = subprocess.run(str(output))
 
