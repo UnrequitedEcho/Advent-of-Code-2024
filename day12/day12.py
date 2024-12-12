9d190ffc1663ac03548c26727f3b78298cd6ea36
UnrequitedEcho <139258164+UnrequitedEcho@users.noreply.github.com>
2024-12-12 15:47:18 +0100

    Day12 python solution cleanup

--- day12/day12.py
+++ day12/day12.py
@@ -8,7 +8,7 @@ with open("day12.txt") as input:
 
 already_assigned = set()
 regions = []
-
+# Populates regions, a list of plots of the same plant type
 for y, line in enumerate(garden_map):
 	for x, plant_type in enumerate(line):
 		if (y, x) in already_assigned:
@@ -31,34 +31,11 @@ for y, line in enumerate(garden_map):
 			already_assigned.add(plot)
 			plots_to_check += [(plot[0]-1, plot[1]), (plot[0]+1, plot[1]), (plot[0], plot[1]-1), (plot[0], plot[1]+1)]
 
-# Part 1
-fences = []
-for region in regions:
-	fences.append(0)
-	for plot in region:
-		adjacent_plots = []
-		for maybe_adjacent in region:
-			if maybe_adjacent == plot:
-				continue
-			if maybe_adjacent[0] == plot[0] and (maybe_adjacent[1] == plot[1]+1 or maybe_adjacent[1] == plot[1]-1):
-				adjacent_plots.append(maybe_adjacent)
-				continue
-			if maybe_adjacent[1] == plot[1] and (maybe_adjacent[0] == plot[0]+1 or maybe_adjacent[0] == plot[0]-1):
-				adjacent_plots.append(maybe_adjacent)
-				continue
-
-		fences[-1] += 4 - len(adjacent_plots)
-
-price = 0
-for f, r in zip(fences, regions):
-	price += f * len(r)
-print(price)
-
-# Part 2
 class Point():
 	def __init__(self, y, x):
 		self.y = y
 		self.x = x
+
 	def __eq__(self, other):
 		return self.x == other.x and self.y == other.y
 
@@ -71,18 +48,12 @@ class Segment():
 			self.p1 = p2
 			self.p2 = p1
 
-	def is_horizontal(self):
-		if self.p1.x == self.p2.x:
-			return True
-
-	def __repr__(self):
-		return "Segment : " + str(self.p1.y) + "," + str(self.p1.x) + "-" + str(self.p2.y) + "," + str(self.p2.x)
-
-fences = []
+part1_fences = []
+part2_fences = []
 for region in regions:
-	print("New Region")
 	segments = set()
 	for plot in region:
+		# generate the list of adjacent plots
 		adjacent_plots = []
 		for maybe_adjacent in region:
 			if maybe_adjacent == plot:
@@ -93,6 +64,7 @@ for region in regions:
 			if maybe_adjacent[1] == plot[1] and (maybe_adjacent[0] == plot[0]+1 or maybe_adjacent[0] == plot[0]-1):
 				adjacent_plots.append(maybe_adjacent)
 				continue
+		# add the fences around each plot if there is no adjacent one
 		TL = Point(plot[0], plot[1])
 		BL = Point(plot[0]+1, plot[1])
 		TR = Point(plot[0], plot[1]+1)
@@ -105,6 +77,10 @@ for region in regions:
 			segments.add(Segment(TL, BL))
 		if (plot[0], plot[1]+1) not in adjacent_plots: # right
 			segments.add(Segment(TR, BR))
+
+	part1_fences.append(len(segments))
+
+	# for part 2, we merge the fences when 1.they have a point in common 2. they are both vertical or both horizontal 3. they aren't delimiting the moebius fencing case
 	old_segments = set()
 	while len(segments) != len(old_segments):
 		old_segments = segments.copy()
@@ -113,25 +89,30 @@ for region in regions:
 				for segment2 in segments:
 					if segment1 == segment2:
 						continue
-					if segment1.p2 == segment2.p1: # point in commont
+					# point in common
+					if segment1.p2 == segment2.p1: 
 						# This is the check for moebius fencing corner case
 						if ((segment1.p2.y, segment1.p2.x) not in region) and ((segment1.p2.y-1, segment1.p2.x-1) not in region):
 							continue
 						if ((segment1.p2.y-1, segment1.p2.x) not in region) and ((segment1.p2.y, segment1.p2.x-1) not in region):
 							continue
 
-						if (segment1.p1.x == segment2.p2.x) or (segment1.p1.y == segment2.p2.y):	# both horizontal or both vertical
+						# both horizontal or both vertical
+						if (segment1.p1.x == segment2.p2.x) or (segment1.p1.y == segment2.p2.y):
 							segment1.p2 = segment2.p2
 							segments.remove(segment2)
 							raise StopIteration
 		except StopIteration:
 			pass
-	print(segments)
-	fences.append(len(segments))
-
+	part2_fences.append(len(segments))
 
 
 price = 0
-for f, r in zip(fences, regions):
+for f, r in zip(part1_fences, regions):
+	price += f * len(r)
+print(price)
+
+price = 0
+for f, r in zip(part2_fences, regions):
 	price += f * len(r)
 print(price)
