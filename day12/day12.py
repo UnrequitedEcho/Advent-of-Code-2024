3afc9bb2152c6bcb07ef972681c0b852e7f10de0
UnrequitedEcho <139258164+UnrequitedEcho@users.noreply.github.com>
2024-12-12 13:04:35 +0100

    day12 python solution, both parts

--- day12/day12.py
+++ day12/day12.py
@@ -1,3 +1,4 @@
+# I refuse to believe this is an optimal solution...
 from collections import Counter
 
 garden_map = []
@@ -30,6 +31,7 @@ for y, line in enumerate(garden_map):
 			already_assigned.add(plot)
 			plots_to_check += [(plot[0]-1, plot[1]), (plot[0]+1, plot[1]), (plot[0], plot[1]-1), (plot[0], plot[1]+1)]
 
+# Part 1
 fences = []
 for region in regions:
 	fences.append(0)
@@ -46,6 +48,88 @@ for region in regions:
 				continue
 
 		fences[-1] += 4 - len(adjacent_plots)
+
+price = 0
+for f, r in zip(fences, regions):
+	price += f * len(r)
+print(price)
+
+# Part 2
+class Point():
+	def __init__(self, y, x):
+		self.y = y
+		self.x = x
+	def __eq__(self, other):
+		return self.x == other.x and self.y == other.y
+
+class Segment():
+	def __init__(self, p1, p2):
+		if (p2.x > p1.x) or (p2.y > p1.y):
+			self.p1 = p1
+			self.p2 = p2
+		else:
+			self.p1 = p2
+			self.p2 = p1
+
+	def is_horizontal(self):
+		if self.p1.x == self.p2.x:
+			return True
+
+	def __repr__(self):
+		return "Segment : " + str(self.p1.y) + "," + str(self.p1.x) + "-" + str(self.p2.y) + "," + str(self.p2.x)
+
+fences = []
+for region in regions:
+	print("New Region")
+	segments = set()
+	for plot in region:
+		adjacent_plots = []
+		for maybe_adjacent in region:
+			if maybe_adjacent == plot:
+				continue
+			if maybe_adjacent[0] == plot[0] and (maybe_adjacent[1] == plot[1]+1 or maybe_adjacent[1] == plot[1]-1):
+				adjacent_plots.append(maybe_adjacent)
+				continue
+			if maybe_adjacent[1] == plot[1] and (maybe_adjacent[0] == plot[0]+1 or maybe_adjacent[0] == plot[0]-1):
+				adjacent_plots.append(maybe_adjacent)
+				continue
+		TL = Point(plot[0], plot[1])
+		BL = Point(plot[0]+1, plot[1])
+		TR = Point(plot[0], plot[1]+1)
+		BR = Point(plot[0]+1, plot[1]+1)
+		if (plot[0]-1, plot[1]) not in adjacent_plots: # above
+			segments.add(Segment(TL, TR))
+		if (plot[0]+1, plot[1]) not in adjacent_plots: # below
+			segments.add(Segment(BL, BR))
+		if (plot[0], plot[1]-1) not in adjacent_plots: # left
+			segments.add(Segment(TL, BL))
+		if (plot[0], plot[1]+1) not in adjacent_plots: # right
+			segments.add(Segment(TR, BR))
+	old_segments = set()
+	while len(segments) != len(old_segments):
+		old_segments = segments.copy()
+		try: 
+			for segment1 in segments:
+				for segment2 in segments:
+					if segment1 == segment2:
+						continue
+					if segment1.p2 == segment2.p1: # point in commont
+						# This is the check for moebius fencing corner case
+						if ((segment1.p2.y, segment1.p2.x) not in region) and ((segment1.p2.y-1, segment1.p2.x-1) not in region):
+							continue
+						if ((segment1.p2.y-1, segment1.p2.x) not in region) and ((segment1.p2.y, segment1.p2.x-1) not in region):
+							continue
+
+						if (segment1.p1.x == segment2.p2.x) or (segment1.p1.y == segment2.p2.y):	# both horizontal or both vertical
+							segment1.p2 = segment2.p2
+							segments.remove(segment2)
+							raise StopIteration
+		except StopIteration:
+			pass
+	print(segments)
+	fences.append(len(segments))
+
+
 
 price = 0
 for f, r in zip(fences, regions):
